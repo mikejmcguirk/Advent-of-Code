f9205ce5a6d2ad7a43e1ac37f3b7bbb1d05b0c22
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2023-12-09 04:04:28 -0500

    Improved cs solution for 2023 D9P2

--- 2023/09/PartTwo.cs
+++ 2023/09/PartTwo.cs
@@ -18,7 +18,18 @@ namespace PartTwo
             {
                 long totalNewNumbers = File.ReadAllLines(input)
                     .Where(line => !string.IsNullOrWhiteSpace(line))
-                    .Aggregate(0L, (total, line) => total + GetNewNum(line));
+                    .Aggregate(0L, (total, line) =>
+                    {
+                        List<long> lineParts = Regex.Split(line, @"\s+")
+                            .Where(s => !string.IsNullOrWhiteSpace(s))
+                            .Select(s => long.Parse(s.Trim(), CultureInfo.InvariantCulture))
+                            .ToList();
+
+                        // long newFirstNumber = lineParts[0] - GetDiff(lineParts); // Part Two
+                        long newFirstNumber = lineParts[^1] + GetDiff(lineParts); // Part One
+
+                        return total + newFirstNumber;
+                    });
 
                 Console.WriteLine("Total new numbers: {0}", totalNewNumbers);
             }
@@ -27,19 +38,6 @@ namespace PartTwo
                 Console.WriteLine("File not found: {0}", input);
                 return;
             }
-        }
-
-        private static long GetNewNum(string line)
-        {
-            List<long> lineParts = Regex.Split(line, @"\s+")
-                .Where(s => !string.IsNullOrWhiteSpace(s))
-                .Select(s => long.Parse(s.Trim(), CultureInfo.InvariantCulture))
-                .ToList();
-
-            long newFirstNumber = lineParts[0] - GetDiff(lineParts); // Part Two
-            // long newFirstNumber = lineParts[^1] + GetDiff(lineParts); // Part One
-
-            return newFirstNumber;
         }
 
         private static long GetDiff(List<long> numbers)
@@ -53,8 +51,7 @@ namespace PartTwo
 
             List<long> diffs = numbers.Zip(numbers.Skip(1), (a, b) => b - a).ToList();
 
-            return diffs[0] - GetDiff(diffs); //Part Two
-            // return diffs[^1] + GetDiff(diffs); // Part One
+            return diffs[^1] + GetDiff(diffs); // Part One
         }
 
     }
