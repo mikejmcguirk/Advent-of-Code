65bf5e28cdabe0a885adcc484f401c388ab52810
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-04-10 22:46:22 -0400

    2015 py 02

---
+++ 2015_py/02/part_one.py
@@ -0,0 +1,19 @@
+lines = [line.strip() for line in open("puzzle_input.txt")]
+
+
+def calc_paper(line):
+    dimensions = [int(dimension) for dimension in line.split("x")]
+    surfaces = [0] * 3
+
+    for i in range(len(dimensions) - 1):
+        for j in range(i + 1, len(dimensions)):
+            surfaces[i + j - 1] = dimensions[i] * dimensions[j]
+
+    paper = sum(surfaces) * 2 + min(surfaces)
+
+    return paper
+
+
+gifts = map(calc_paper, lines)
+total_paper = sum(gifts)
+print(total_paper)
---
+++ 2015_py/02/part_two.py
@@ -0,0 +1,15 @@
+import math as m
+
+lines = [line.strip() for line in open("puzzle_input.txt")]
+
+
+def calc_ribbon(line):
+    dimensions = [int(dimension) for dimension in line.split("x")]
+    dimensions.sort()
+
+    return dimensions[0] * 2 + dimensions[1] * 2 + m.prod(dimensions)
+
+
+gifts = map(calc_ribbon, lines)
+total_ribbon = sum(gifts)
+print(total_ribbon)
