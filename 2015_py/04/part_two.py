916053d181f362b98cb8ac28df9f0369ce1530cc
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-04-11 00:13:18 -0400

    2015 py 04

---
+++ 2015_py/04/part_two.py
@@ -0,0 +1,17 @@
+import hashlib as h
+
+lines = [line.strip() for line in open("puzzle_input.txt")]
+idx = 0
+
+while True:
+    test = lines[0] + str(idx)
+    hashed = h.md5()
+    hashed.update(test.encode())
+    digested = hashed.hexdigest()
+
+    if digested[:6] == "000000":
+        print(digested)
+        print(idx)
+        break
+    else:
+        idx += 1
