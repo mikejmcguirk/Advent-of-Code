d184b37013a43bf720fadcd4c4f3c7252b3e6a36
Mike J. McGuirk <mike.j.mcguirk@gmail.com>
2024-12-01 01:37:36 -0500

    Added revised AOC 2024_py Day01 solutions

---
+++ 2024_py/01/part_one_revised.py
@@ -0,0 +1,18 @@
+lines = [line.strip() for line in open("puzzle_input.txt")]
+
+left_list = []
+right_list = []
+
+for line in lines:
+    left_num, right_num = line.split()
+    left_list.append(int(left_num))
+    right_list.append(int(right_num))
+
+left_list.sort()
+right_list.sort()
+
+distance = 0
+for a, b in zip(left_list, right_list):
+    distance += abs(a - b)
+
+print(distance)
---
+++ 2024_py/01/part_two_revised.py
@@ -0,0 +1,23 @@
+lines = [line.strip() for line in open("puzzle_input.txt")]
+
+left_list = []
+right_list = []
+
+for line in lines:
+    left_num, right_num = line.split()
+    left_list.append(int(left_num))
+    right_list.append(int(right_num))
+
+left_map = {}
+for num in right_list:
+    if num in left_map:
+        left_map[num] += 1
+    else:
+        left_map[num] = 1
+
+score = 0
+for num in left_list:
+    if num in left_map:
+        score += num * left_map[num]
+
+print(score)
